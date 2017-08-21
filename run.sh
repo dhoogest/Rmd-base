8c9a1184874feda616f082291a2cddb0875793a3
Daniel Hoogestraat <dhoogest@uw.edu>
2017-08-21 15:17:33 -0700

    fixed permissions, updated filename in run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ singularity exec \
 	    -B $(pwd):$(pwd) \
 	    -W $(pwd) \
 	    /molmicro/common/singularity/r-reporting-0.3.img \
-	    bin/knit.R classification_counts.Rmd \
-	    --outfile classification_counts.html
+	    bin/knit.R example-report.Rmd \
+	    --outfile example-report.html
